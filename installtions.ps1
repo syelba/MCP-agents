9f308349ec1054d1a81c7625da3cc9a6d886ca10
syelba <160588882+syelba@users.noreply.github.com>
2026-02-17 17:04:21 +0200

    Create installtions.ps1

---
+++ installtions.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop"
+
+$python = "C:\Users\itl\Desktop\MCP\ovms\ovms\python\python.exe"
+$hf_cli = "C:\Users\itl\Desktop\MCP\ovms\ovms\python\Scripts\huggingface-cli.exe"
+
+$proxy = "http://proxy-dmz.intel.com:912"
+
+$modelDir = "C:\Users\itl\Desktop\MCP\ov_model"
+
+Write-Host "Setting proxy..."
+$env:HTTP_PROXY = $proxy
+$env:HTTPS_PROXY = $proxy
+
+Write-Host "Installing huggingface CLI if needed..."
+& $python -m pip install "huggingface_hub[cli]" --proxy $proxy
+
+Write-Host "Downloading ready OpenVINO model..."
+
+& $hf_cli download OpenVINO/Phi-3-mini-4k-instruct-int4-ov `
+  --local-dir $modelDir `
+  --local-dir-use-symlinks False
+
+Write-Host "Checking files..."
+
+if (Test-Path "$modelDir\openvino_model.xml") {
+    Write-Host "SUCCESS - Model ready."
+} else {
+    Write-Host "ERROR - Model not found."
+    exit 1
+}
+
+Write-Host "DONE."
